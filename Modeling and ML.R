9e86036765438ae83a1f3bc9b4718ace49c31219
AntonYurievNikolov <40524852+AntonYurievNikolov@users.noreply.github.com>
2018-07-02 20:50:03 +0300

    Added few SHOCKING discoveries

--- Modeling and ML.R
+++ Modeling and ML.R
@@ -5,13 +5,33 @@ library(tidyr)
 library(purrr)
 library(broom)
 # Perform a linear regression on each item in the data column
-coefficent<-by_year_country %>%
-  nest(-country) %>%
-  mutate(model = map(data, ~ lm(percent_yes ~ year, data = .)))%>%
+coefficent<-Bulgaria %>%
+  select(Salary,FormalEducation,IDE) %>%
+  nest(-IDE) %>%
+  mutate(model = map(data, ~ lm(Salary ~ FormalEducation, data = .)))%>%
   mutate(tidied = map(model , tidy)) %>%
   unnest(tidied)
 
-#mutate(p.adjusted  = p.adjust(p.value))%>%filter(p.adjusted <0.05)
+coefficent%>%mutate(p.adjusted  = p.adjust(p.value))%>%filter(p.adjusted <0.05)
+
+#check this for the whole world
+#WOW
+coefficentWorld<-WorldData %>%
+  select(Salary,FormalEducation,Country) %>%
+  filter(!is.na(Country),!is.na(FormalEducation),!is.na(Salary))%>%
+  group_by(Country)%>%
+  mutate(n = n())%>%
+  filter(n>100)%>%
+  ungroup()%>%
+  mutate(FormalEducation = as.factor(FormalEducation),Country = as.factor(Country))%>%
+  nest(-Country) %>%
+  #filter(Country=="United Kingdom") %>%
+  mutate(
+         model = map(data, ~ lm(Salary ~ FormalEducation, data = .)))%>% 
+         mutate(tidied = map(model , tidy))  %>% 
+  unnest(tidied)
+
+coefficentWorld%>%mutate(p.adjusted  = p.adjust(p.value))%>%filter(p.adjusted <0.05)
 #cut example
 ggplot(data = Bulgaria, 
        aes(x = cut(Bulgaria$YearsCoding , breaks = 3), y = Bulgaria$Salary)) + 
@@ -23,15 +43,22 @@ Bulgaria %>%
 
 cor<-cor( Bulgaria$Salary,Bulgaria$YearsCodingProf, use = "pairwise.complete.obs")
 mod<-lm(Salary~YearsCodingProf,data = Bulgaria)
-summary(mod)
-fitted.values(mod)
-augment(mod)
-
-mean(fitted.values(mod)) - mean(Bulgaria$Salary) < 200
-mean(residuals(mod))
+#predicting
 predict(mod,newdata = Bulgaria[Bulgaria$YearsCodingProf == 12,] %>% select(Salary,YearsCodingProf))
 Salary<-c(4800)
 YearsCodingProf<-c(11)
 test<-data.frame(Salary,YearsCodingProf)
 predict(mod,newdata =test)
 #fitting the alogorythm
+summary(mod)
+fitted.values(mod)
+aug<-as.tbl( augment(mod))
+
+mean(fitted.values(mod)) - mean(Bulgaria$Salary) < 200
+mean(residuals(mod))
+#RMSE
+sqrt(sum(residuals(mod)^2) / df.residual(mod))
+
+#R2 - how much is explained
+1 - var(aug$.resid)/var(aug$Salary) 
+
